73d468700ddb74fd084a9c4243de2593c087933d
Jurek <jurek2006@gmail.com>
2018-07-09 12:12:50 +0200

    Message Generator & Tests

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const {generateMessage} = require('./utils/message')
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -16,27 +17,16 @@ io.on('connection', (socket) => {
     console.log('New user connected');
 
     // emitowanie wiadomości powitalnej do połączonego użytkownika
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to the chat',
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat'));
 
     // emitowanie wiadomości, że (inny) użytkownik się połączył
-    socket.broadcast.emit('newMessage', {
-        text: 'New user joined',
-        createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage', generateMessage('New user joined', 'New user joined'));
 
     socket.on('createMessage', message => {
         console.log('create message', message);
 
         // emitowanie newMessage do wszystkich
-        io.emit('newMessage', {
-            from: message.from, 
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
     });
 
     socket.on('disconnect', () => {
