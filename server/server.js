006e9a69cb02b440dbfeab5f4a39855bd209d61e
Jurek <jurek2006@gmail.com>
2018-07-09 11:09:35 +0200

    Broadcasting Events

--- server/server.js
+++ server/server.js
@@ -15,13 +15,28 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    // emitowanie wiadomości powitalnej do połączonego użytkownika
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat',
+        createdAt: new Date().getTime()
+    });
+
+    // emitowanie wiadomości, że (inny) użytkownik się połączył
+    socket.broadcast.emit('newMessage', {
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', message => {
         console.log('create message', message);
+
+        // emitowanie newMessage do wszystkich
         io.emit('newMessage', {
             from: message.from, 
             text: message.text,
             createdAt: new Date().getTime()
-        })
+        });
     });
 
     socket.on('disconnect', () => {
